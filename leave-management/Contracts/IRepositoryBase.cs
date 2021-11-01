f584aabd2ed32f87c53cb187c6bdba9689b5aabb
sebas <sebas@DESKTOP-7QQVDBB>
2021-11-01 23:55:13 +0000

    Add project files.

---
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IRepositoryBase<T> where T : class  // It means this interface is generic enough that I can pass any class in here
+    {                                                    // and that class (table) should be able to perform any operations I want (CRUD)
+        ICollection<T> FindAll();
+        T FindById(int id);                              // Icollection is a very generic class kind of like list, but it can be almost
+        bool Create(T entity);                           // Icollection is a very generic class kind of like list, but it can be almost
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool Save();
+    }                                                   
+}
---
+++ leave-management/Data/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string TaxID { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+        
+        public DateTime DateCreated { get; set; }
+
+    }
+}
