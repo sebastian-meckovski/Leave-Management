417e58f1edbe50f1660f7e8c29c0f07b9c17be23
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2021-11-07 13:08:56 +0000

    Index view has been set up.  Mapped the data coming from the db to the viewmodel structure and then passed that model to View method. Generated LeaveType view inde.cshtml which is now accessible when running project

--- leave-management/Data/ApplicationDbContext.cs
+++ leave-management/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using leave_management.Models;
 
 namespace leave_management.Data
 {
@@ -20,5 +21,7 @@ namespace leave_management.Data
         public DbSet<LeaveType> LeaveTypes { get; set; }
 
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        public DbSet<leave_management.Models.DetailsLeaveTypeVM> DetailsLeaveTypeVM { get; set; }
     }
 }
