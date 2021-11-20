4fee460e4b20947c4992bc1d732f408480fa37e5
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2021-11-20 15:02:41 +0000

    more bootstrap styling added on LeaveTypes index - improved buttons

--- leave-management/wwwroot/js/site.js
+++ leave-management/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//btw this j-query
+$(document).ready(function () {
+    $('#tblData').DataTable();
+});
